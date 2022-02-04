b9e4b60e5e24e42404f25dc1697dfbe4c90b8b2e
gabrielrodrig0 <gabrielrodrigonaga@gmail.com>
2022-02-04 17:53:41 -0300

    primeiro commit

---
+++ Desafio1.js
@@ -0,0 +1,17 @@
+//Tarefa de média - Gabriel Rodrigo
+
+function media (array){
+
+    let sum = 0;
+    
+    for (let k =0; k<arguments.length; k++){
+      sum+= arguments[k];
+    }
+    
+    
+    let media = sum/arguments.length;
+      
+    console.log(media);
+    }
+    
+    media (2,2,2);
